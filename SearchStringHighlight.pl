e66197180d3e4a4cc9e976c0c6f5b151b86618de
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2012-04-20 10:35:31 +0000

    * バージョン番号とか微調整
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/SearchStringHighlight/trunk@211 adbff654-c4ea-448a-8ae3-cad037306009

--- SearchStringHighlight.pl
+++ SearchStringHighlight.pl
@@ -1,26 +1,28 @@
-package MT::Plugin::OMV::SearchStringHighlight;
+package MT::Plugin::Search::OMV::SearchStringHighlight;
+# $Id$
 
 use strict;
-use MT 4;
+use MT 5;
 use MT::Util;
 
-use vars qw( $MYNAME $VERSION );
-$MYNAME = 'SearchStringHighlight';
-$VERSION = '0.20';
+use vars qw( $VENDOR $MYNAME $VERSION );
+($VENDOR, $MYNAME) = (split /::/, __PACKAGE__)[-2, -1];
+(my $revision = '$Rev$') =~ s/\D//g;
+$VERSION = "0.20_$revision";
 
 use base qw( MT::Plugin );
-my $plugin = __PACKAGE__->new({
+my $plugin = __PACKAGE__->new ({
     id => $MYNAME,
     key => $MYNAME,
     name => $MYNAME,
     version => $VERSION,
     author_name => 'Open MagicVox.net',
     author_link => 'http://www.magicvox.net/',
-    doc_link => 'http://www.magicvox.net/archive/2010/05152010/',
+    plugin_link => 'http://www.magicvox.net/archive/2010/05152010/', # blog
+    doc_link => 'http://lab.magicvox.net/trac/mt-plugins/wiki/SearchStringHighlight', # trac
     description => <<PERLHEREDOC,
 <__trans phrase="Highlight the matched string in the entry.">
 PERLHEREDOC
-#    l10n_class => $MYNAME. '::L10N',
     registry => {
         tags => {
             function => {
@@ -45,24 +47,26 @@ sub tag_searchstringhighlight {
     my $text = ($entry->text || ''). ($entry->text_more || '');
     $text = MT::Util::remove_html ($text);
 
-    # Matching
+    # Arguments
     my $ql1 = $args->{'length'} || 20;
+    my $blog = $ctx->stash('blog');
+    my $words = $args->{words} || ($blog ? $blog->words_in_excerpt : 40);
+    my $tag = $args->{tag} || 'b';
+    $args->{cite} ||= 5;
+
+    # Matching
     my @matches;
     while ($text =~ s/((.{0,$ql1}$needle)+.{0,$ql1})//i) {
         push @matches, $1;
     }
-
-    # Limit the number of citing
-    $args->{cite} ||= 5;
+    #
     pop @matches while $args->{cite} < @matches;
 
-    # Make a string
     $text = @matches
         ? join '...', '', @matches, ''
         : $entry->get_excerpt;
 
     # Highlight with tag
-    my $tag = $args->{tag} || 'b';
     $text =~ s!($needle)!<$tag>$1</$tag>!gsi;
 
     $text;
